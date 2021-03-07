2a3eb2d8df3dcbcce540651eabdcda84f1e53c60
Thomas <hakerh403@gmail.com>
2021-03-08 00:50:02 +0100

    commit

--- projects/tags/main.js
+++ projects/tags/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 await O.addStyle('style.css');
+
+const BASE_URL = path.join(O.baseURL, '..');
+const BASE_PTH = 'D:';
+
+const INIT_URL = path.join(BASE_URL, 'Videos/Other/Folder');
 
 const sem = new O.Semaphore();
 
-const main = () => {
+const main = async () => {
   const {body} = O;
 
   const ael = (target, type, handler) => {
@@ -28,20 +36,155 @@ const main = () => {
     });
   };
 
-  const input = O.ce(body, 'input');
+  const getAllInputFields = () => {
+    return [...O.qsa('input')];
+  };
 
-  ael(input, 'input', async evt => {
-    const tags = input.value.trim().split(/\s*,\s*/);
-    const substr = O.last(tags);
+  const next = () => {
+    const inputs = getAllInputFields();
+    const index = inputs.indexOf(document.activeElement);
+    const indexNew = (index + 1) % inputs.length;
 
-    if(substr === ''){
-      setResult();
+    inputs[indexNew].focus();
+  };
+
+  const ceInput = (label, onInput=null, onTab=null, onEnter=null) => {
+    const inputWrap = O.ceDiv(body, 'input-wrap');
+    const labelDiv = O.ceDiv(inputWrap, 'label');
+    labelDiv.innerText = `${label}:`;
+
+    const input = O.ce(inputWrap, 'input');
+
+    if(onInput !== null){
+      ael(input, 'input', onInput);
+      ael(input, 'focus', onInput);
+    }
+
+    ael(input, 'keydown', async evt => {
+      if(evt.ctrlKey) return;
+      if(evt.altKey) return;
+
+      if(evt.code === 'Tab'){
+        O.pd(evt);
+
+        if(!evt.shiftKey && onTab !== null)
+          return await onTab(evt);
+
+        return next();
+      }
+
+      if(evt.code === 'Enter' || evt.code === 'NumpadEnter'){
+        if(onEnter !== null)
+          return await onEnter(evt);
+
+        return next();
+      }
+    });
+
+    return input;
+  };
+
+  const setFilePth = async pth => {
+    pth = pth.replace(/\/$/, '');
+
+    const file = url2pth(pth);
+
+    if(await O.rmi('fs.isDir', file)){
+      inputFilePth.value = `${pth}/`;
+      await updateFileMatches();
       return;
     }
 
-    const matches = await O.rmi('tags.tag.search', substr);
+    const link = O.doc.createElement('a');
+    link.href = pth;
+    link.target = '_blank';
+    link.click();
 
-    setResult(matches.join('\n'));
+    inputFilePth.value = pth;
+
+    const tags = await O.rmi('tags.file.getTags', file);
+
+    if(tags !== null)
+      setInputTags(tags);
+
+    next();
+  };
+
+  const updateFileMatches = async () => {
+    const pth = inputFilePth.value;
+    const items = await O.rmi('tags.fs.readdir', url2pth(pth));
+
+    setMatches(items);
+  };
+
+  const inputFilePth = ceInput('File', updateFileMatches, async evt => {
+    const pth = inputFilePth.value;
+
+    if(pth.endsWith('/.')){
+      await setFilePth(path.join(pth, '..'));
+      return;
+    }
+
+    if(matches.length === 0) return;
+
+    const m = matches[0];
+    const sep = pth.endsWith('/') ? '.' : '..';
+
+    await setFilePth(path.join(pth, sep, m));
+  });
+
+  inputFilePth.value = `${INIT_URL}/`;
+  inputFilePth.focus();
+
+  ael(inputFilePth, 'blur', async evt => {
+    setLinks();
+  });
+
+  const getInputTags = (includeEmpty=1) => {
+    let tags = inputTags.value.trim().split(/\s*,\s*/);
+
+    if(!includeEmpty)
+      tags = tags.filter(a => a);
+
+    return tags;
+  };
+
+  const setInputTags = tags => {
+    inputTags.value = [...tags, ''].join(', ');
+  };
+
+  const updateTagList = async () => {
+    const tags = getInputTags();
+    const substr = O.last(tags);
+
+    if(substr === ''){
+      let links = await O.rmi('tags.file.search', tags.slice(0, tags.length - 1));
+
+      links = links.map(link => pth2url(link));
+
+      setMatches([]);
+      setLinks(links);
+    }else{
+      const matches = await O.rmi('tags.tag.search', substr);
+
+      setMatches(matches);
+      setLinks();
+    }
+  };
+
+  const inputTags = ceInput('Tags', updateTagList, async evt => {
+    const tags = getInputTags();
+
+    if(matches.length !== 0)
+      O.setLast(tags, matches[0]);
+
+    setInputTags(tags);
+    await updateTagList();
+  }, async evt => {
+    const tags = getInputTags(0);
+    const file = url2pth(inputFilePth.value);
+
+    await O.rmi('tags.file.setTags', file, tags);
   });
 
   const ceBtn = (label, handler) => {
@@ -52,22 +195,36 @@ const main = () => {
     ael(btn, 'click', handler);
   };
 
-  ceBtn('New tag', async evt => {
-    const tag = 'aaa'//prompt('Enter tag:');
-    if(tag === null) return;
+  O.ceBr(body);
 
-    await O.rmi('tags.tag.new', tag);
+  const resultLeft = O.ceDiv(body, 'result result-left');
+  const resultRight = O.ceDiv(body, 'result result-right');
 
-    setResult(`Added new tag: ${O.sf(tag)}`);
-  });
+  let matches = [];
 
-  ceBtn('List tags');
-
-  const result = O.ceDiv(body, 'result');
+  const setMatches = arr => {
+    matches = arr;
+    setResult(arr.join('\n'));
+  };
 
   const setResult = (str='') => {
-    result.innerText = str;
+    resultLeft.innerText = str;
+  };
+
+  const setLinks = (links=[]) => {
+    resultRight.innerText = '';
+
+    for(const link of links)
+      O.ceLink(resultRight, link, link);
   };
 };
 
-main();
+const url2pth = url => {
+  return url.replace(BASE_URL, BASE_PTH);
+};
+
+const pth2url = pth => {
+  return pth.replace(BASE_PTH, BASE_URL);
+};
+
+main().catch(log);
