5bee44c9911b64201ba8251f5a79e40eddafcf35
Thomas <hakerh403@gmail.com>
2021-06-21 18:13:18 +0200

    commit

--- projects/math/main.js
+++ projects/math/main.js
@@ -4,8 +4,12 @@ const mathjax = require('./mathjax');
 
 await O.addStyle('style.css');
 
+const mainDiv = O.ceDiv(O.body, 'main');
+const mainFrame = O.ceDiv(mainDiv, 'main-frame');
+const locationBar = O.ceDiv(mainFrame, 'location-bar');
+const consoleDiv = O.ceDiv(mainDiv, 'console hidden');
+
 const main = () => {
-  // Test
 };
 
 main();
