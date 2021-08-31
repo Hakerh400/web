b4525a6b45f5164e18b1560d511711222882b8f2
Hakerh400 <hakerh403@gmail.com>
2021-08-31 12:16:17 +0200

    commit

--- projects/states/main.js
+++ projects/states/main.js
@@ -1,7 +1,38 @@
 'use strict';
 
+const assert = require('assert');
+
+const cols = {
+  bg: 'darkgray',
+};
+
+const {g} = O.ceCanvas(1);
+
+let iw, ih;
+let iwh, ihh;
+
 const main = () => {
-  
+  aels();
+  onResize();
+};
+
+const aels = () => {
+  O.ael('resize', onResize);
+};
+
+const onResize = evt => {
+  iw = O.iw;
+  ih = O.ih;
+  iwh = iw / 2;
+  ihh = ih / 2;
+
+  g.resize(iw, ih);
+
+  render();
+};
+
+const render = () => {
+  g.clearCanvas(cols.bg);
 };
 
 main();
